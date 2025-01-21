40715b87a0cc36f2b4b3b0b1ec90625d74305416
David Eastmond <davebuck1981@gmail.com>
2025-01-21 18:40:32 -0500

    linting, add github

--- src/auth.ts
+++ src/auth.ts
@@ -1,24 +1,27 @@
 import connectDb from "@/lib/mongodb/mongodb";
 import { UserModel } from "@/lib/schemas/user.schema";
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import NextAuth, { Account, Profile } from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google, { GoogleProfile } from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Google],
+  providers: [Google, GitHub],
   callbacks: {
-    signIn: async ({ profile }) => {
-      // Check if the user exists in our database, if they don't, add them
+    signIn: async ({ account, profile }) => {
+      if (!profile) return false;
+      if (!account) return false;
 
       try {
         await connectDb();
         const existingUser = await UserModel.findOne({
-          where: { email: profile?.email },
+          email: profile.email,
         });
 
         if (!existingUser) {
+          // Check if the user exists in our database, if they don't, add them
           await UserModel.create({
-            email: profile?.email,
-            name: profile?.name,
+            email: profile.email,
+            name: getUsernameFromProfile(account, profile),
           });
         }
       } catch (error) {
@@ -30,3 +33,15 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
   },
 });
+
+function getUsernameFromProfile(account: Account, profile: Profile): string {
+  if (account.provider === "google") {
+    // For Google, combine the first and last name
+    return `${(profile as GoogleProfile).given_name} ${
+      (profile as GoogleProfile).family_name
+    }`;
+  }
+
+  // Otherwise, just use the name
+  return profile.name!;
+}
