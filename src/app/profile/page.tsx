81a5470f780b70849941783a3385bd062f8238ae
David Eastmond <davebuck1981@gmail.com>
2025-01-22 17:50:52 -0500

    update import for auth

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,26 +1,34 @@
-import { redirect } from 'next/navigation';
-import { signOut, auth } from '../../auth';
+import { auth, signOut } from "@/auth/auth";
+import { redirect } from "next/navigation";
 
 export default async function Profile() {
   const session = await auth();
-  const user = session?.user
+  const user = session?.user;
 
-  return (
-    user ?
+  return user ? (
     <div className={"flex flex-col items-center w-full"}>
       <h1 className={"font-bold text-lg"}>Welcome {user.name}</h1>
-      { user.image && <img className={"rounded-full border border-black h-16 w-16 my-4"} src={user.image} alt={"User image"} /> }
-      <form action={async () => {
-        "use server";
-        await signOut();
-      }}>
+      {user.image && (
+        <img
+          className={"rounded-full border border-black h-16 w-16 my-4"}
+          src={user.image}
+          alt={"User image"}
+        />
+      )}
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
         <button className="relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Sign out</span>
+            Sign out
+          </span>
         </button>
       </form>
     </div>
-    :
-    redirect('/')
+  ) : (
+    redirect("/")
   );
 }
