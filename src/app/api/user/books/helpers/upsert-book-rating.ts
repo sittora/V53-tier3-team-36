ffe800a55f40e4d8d35d6edc10b8ddcd14923092
David Eastmond <davebuck1981@gmail.com>
2025-01-23 18:18:09 -0500

    add helper for book rating

---
+++ src/app/api/user/books/helpers/upsert-book-rating.ts
@@ -0,0 +1,22 @@
+import { BookModel } from "@/lib/schemas/book.schema";
+
+export async function upsertBookRating(
+  olid: string,
+  userId: string,
+  rating: number
+): Promise<void> {
+  // Check if the book exists, if not create it and add the rating and user info to it
+
+  const targetBook = await BookModel.findOne({ olid });
+  if (!targetBook) {
+    await BookModel.create({
+      olid,
+      userRatings: { [userId]: rating },
+    });
+    return;
+  }
+
+  const { userRatings } = targetBook;
+  userRatings.set(userId, rating);
+  await targetBook.save();
+}
