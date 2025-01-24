baca047bf5bd02fccaf852dbe8c645ee85d14c18
David Eastmond <davebuck1981@gmail.com>
2025-01-23 19:07:18 -0500

    tweak function names in the book client

--- src/app/clients/book-client.ts
+++ src/app/clients/book-client.ts
@@ -21,8 +21,8 @@ export const BookClient = {
       throw new Error("Failed to mark book as read");
     }
   },
-  markUnRead: async (olid: string) => {
-    // This allows user to mark a book as unread
+  undoMarkRead: async (olid: string) => {
+    // This allows user to undo the 'mark as read' action
     const response = await fetch("/api/user/books/read", {
       method: "PATCH",
       body: JSON.stringify({ olid, action: "mark-unread" }),
@@ -33,8 +33,8 @@ export const BookClient = {
       throw new Error("Failed to mark book as unread");
     }
   },
-  markUndoWantToRead: async (olid: string) => {
-    // This allows user to undo a want to read action
+  undoMarkWantToRead: async (olid: string) => {
+    // This allows user to undo the 'want to read' action
     const response = await fetch("/api/user/books/want-to-read", {
       method: "PATCH",
       body: JSON.stringify({ olid, action: "mark-undo-want-to-read" }),
