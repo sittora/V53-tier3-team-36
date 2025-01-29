e738b39cdc58403b67c9a2a912e7abba85395b90
Abigail Swarth <abigailswarth@gmail.com>
2025-01-29 12:19:51 -0500

    book card fixes

--- src/components/trendingBooks/BookCard.tsx
+++ src/components/trendingBooks/BookCard.tsx
@@ -1,12 +1,12 @@
-import Link from '../../../node_modules/next/link';
+import Link from 'next/link';
 
-export default function BookCard(props: {title: any, author: any, id: number}) {
+export default function BookCard({ title, author, id} : { title: string, author: string, id: number } ) {
 
     return (
     <div className="flex border rounded-md border-black flex-col p-3 m-4 items-center justify-center">
-        <span className="text-center font-bold">{props.title}</span>
-        {props.author ? <span className="text-center">by {props.author}</span> : null}
-        <Link href={`/?showDialog=y&search=${props.id}`} className="underline text-center">
+        <span className="text-center font-bold">{title}</span>
+        {author ? <span className="text-center">by {author}</span> : null}
+        <Link href={`/?showDialog=y&search=${id}`} className="underline text-center">
         View Book Details
       </Link>
     </div>
