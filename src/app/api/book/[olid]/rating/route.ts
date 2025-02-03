62e0ffcb7daa1b1205b5f41402be0d8724d258c1
David Eastmond <davebuck1981@gmail.com>
2025-02-02 21:09:45 -0500

    rating functionality

---
+++ src/app/api/book/[olid]/rating/route.ts
@@ -0,0 +1,46 @@
+import { auth } from "@/auth/auth";
+import { BookModel } from "@/lib/schemas/book.schema";
+import { NextResponse } from "next/server";
+
+type RouteParams = Record<string, string>;
+
+// This route gets the average rating of a book and the user's rating
+export const GET = auth(async function GET(req, { params }) {
+  if (!req.auth || !req.auth.user) {
+    return NextResponse.json({ error: "Not authorized" }, { status: 401 });
+  }
+  const { olid } = (await params) as RouteParams;
+
+  // Find the book in the Books collection - there is a chance the book may not exist. If it doesn't create it
+  const book = await BookModel.findOne({ olid: `/works/${olid}` });
+
+  // If the book doesn't exist, there is no raiting info, so return null.
+  if (!book)
+    return NextResponse.json(
+      { averageRating: null, userRating: null },
+      { status: 200 }
+    );
+
+  // Find the authenticated user's rating if it exists
+  const userRating = book.userRatings.get(req.auth.user.id as string) ?? null;
+
+  // If the book has no ratings, short circuit and return null
+  if (!book.userRatings || book.userRatings.size === 0) {
+    return NextResponse.json(
+      { averageRating: null, userRating },
+      { status: 200 }
+    );
+  }
+
+  let ratingsSum = 0;
+  for (const [, value] of book.userRatings) {
+    ratingsSum += value;
+  }
+  return NextResponse.json(
+    {
+      userRating,
+      averageRating: Math.floor(ratingsSum / book.userRatings.size),
+    },
+    { status: 200 }
+  );
+});
