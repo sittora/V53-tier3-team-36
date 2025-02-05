9c38e5dac6b96db7bc298e503e68a1ec1e371228
Abigail Swarth <33330273+abby-wankenobi@users.noreply.github.com>
2025-02-05 09:10:27 -0800

    Simplify boolean
    Co-authored-by: David Eastmond <davebuck1981@gmail.com>

--- src/components/pageLayout/PageLayout.tsx
+++ src/components/pageLayout/PageLayout.tsx
@@ -23,7 +23,7 @@ export default async function PageLayout({
         {children}
       </div>
       <Footer />
-      <Dialog loggedIn={user === undefined ? false : true} />
+      <Dialog loggedIn={Boolean(user)} />
     </div>
   );
 }
